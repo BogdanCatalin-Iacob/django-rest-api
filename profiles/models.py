5cf94dc0d8d6fb2fcb593569244a53f67d44b647
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2023-11-29 03:08:52 +0000

    Refactor typo fix created_at

--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    crteated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     content = models.TextField(blank=True, null=True)

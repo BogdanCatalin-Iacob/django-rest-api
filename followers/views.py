61139aae5f53c8887a635809337ed2ecadd3e952
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2023-12-14 20:07:17 +0000

    Create FollowerList view

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django_rest_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerialzier
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    '''
+    List all followers.
+    Create a follower.
+    perform_create method associate the current logged user woth a follower
+    '''
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerialzier
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(ownr=self.request.user)
+    

32201b9e460ccc734d0a5941f8acce1b53ea039c
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2023-12-18 06:24:01 +0000

    Refactor: add permission import deleted by mistake

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from rest_framework import generics, filters
+from rest_framework import generics, permissions, filters
 from django_rest_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer

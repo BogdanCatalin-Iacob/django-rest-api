27827eba0a499e197256406401aeb404c905df65
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2023-12-18 04:05:23 +0000

    Refactor: annotate profile list queryset to get specific fields

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, filters
 from django_rest_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -10,7 +11,13 @@ class ProfileList(generics.ListAPIView):
     No profile creation as it is handled by django signals
     '''
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+
+    # add specific fields to queryset
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):

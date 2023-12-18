562a7f661023808f39fddeeee5083c7c315b743c
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2023-12-18 04:08:03 +0000

    Create ordering filter

--- profiles/views.py
+++ profiles/views.py
@@ -19,6 +19,17 @@ class ProfileList(generics.ListAPIView):
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
 
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed_created_at'
+    ]
+
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     '''
